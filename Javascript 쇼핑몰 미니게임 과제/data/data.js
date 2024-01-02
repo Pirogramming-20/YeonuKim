3ba2070506b081dc52b6c5a3fc62a2cf8caec42a
yeonuKim <ywk0524@snu.ac.kr>
2024-01-02 23:04:17 +0900

    [Let's get started] Make a page myself

---
+++ Javascript 쇼핑몰 미니게임 과제/data/data.js
@@ -0,0 +1,268 @@
+dataFile = {
+    "product": [
+  {
+    "image": "blue_p.png",
+    "sex": "female",
+    "size": "small"
+  },
+  {
+    "image": "blue_p.png",
+    "sex": "female",
+    "size": "medium"
+  },
+  {
+    "image": "blue_p.png",
+    "sex": "female",
+    "size": "large"
+  },
+  {
+    "image": "blue_p.png",
+    "sex": "male",
+    "size": "small"
+  },
+  {
+    "image": "blue_p.png",
+    "sex": "male",
+    "size": "medium"
+  },
+  {
+    "image": "blue_p.png",
+    "sex": "male",
+    "size": "large"
+  },
+  {
+    "image": "blue_s.png",
+    "sex": "female",
+    "size": "small"
+  },
+  {
+    "image": "blue_s.png",
+    "sex": "female",
+    "size": "medium"
+  },
+  {
+    "image": "blue_s.png",
+    "sex": "female",
+    "size": "large"
+  },
+  {
+    "image": "blue_s.png",
+    "sex": "male",
+    "size": "small"
+  },
+  {
+    "image": "blue_s.png",
+    "sex": "male",
+    "size": "medium"
+  },
+  {
+    "image": "blue_s.png",
+    "sex": "male",
+    "size": "large"
+  },
+  {
+    "image": "blue_t.png",
+    "sex": "female",
+    "size": "small"
+  },
+  {
+    "image": "blue_t.png",
+    "sex": "female",
+    "size": "medium"
+  },
+  {
+    "image": "blue_t.png",
+    "sex": "female",
+    "size": "large"
+  },
+  {
+    "image": "blue_t.png",
+    "sex": "male",
+    "size": "small"
+  },
+  {
+    "image": "blue_t.png",
+    "sex": "male",
+    "size": "medium"
+  },
+  {
+    "image": "blue_t.png",
+    "sex": "male",
+    "size": "large"
+  },
+  {
+    "image": "pink_p.png",
+    "sex": "female",
+    "size": "small"
+  },
+  {
+    "image": "pink_p.png",
+    "sex": "female",
+    "size": "medium"
+  },
+  {
+    "image": "pink_p.png",
+    "sex": "female",
+    "size": "large"
+  },
+  {
+    "image": "pink_p.png",
+    "sex": "male",
+    "size": "small"
+  },
+  {
+    "image": "pink_p.png",
+    "sex": "male",
+    "size": "medium"
+  },
+  {
+    "image": "pink_p.png",
+    "sex": "male",
+    "size": "large"
+  },
+  {
+    "image": "pink_s.png",
+    "sex": "female",
+    "size": "small"
+  },
+  {
+    "image": "pink_s.png",
+    "sex": "female",
+    "size": "medium"
+  },
+  {
+    "image": "pink_s.png",
+    "sex": "female",
+    "size": "large"
+  },
+  {
+    "image": "pink_s.png",
+    "sex": "male",
+    "size": "small"
+  },
+  {
+    "image": "pink_s.png",
+    "sex": "male",
+    "size": "medium"
+  },
+  {
+    "image": "pink_s.png",
+    "sex": "male",
+    "size": "large"
+  },
+  {
+    "image": "pink_t.png",
+    "sex": "female",
+    "size": "small"
+  },
+  {
+    "image": "pink_t.png",
+    "sex": "female",
+    "size": "medium"
+  },
+  {
+    "image": "pink_t.png",
+    "sex": "female",
+    "size": "large"
+  },
+  {
+    "image": "pink_t.png",
+    "sex": "male",
+    "size": "small"
+  },
+  {
+    "image": "pink_t.png",
+    "sex": "male",
+    "size": "medium"
+  },
+  {
+    "image": "pink_t.png",
+    "sex": "male",
+    "size": "large"
+  },
+  {
+    "image": "yellow_p.png",
+    "sex": "female",
+    "size": "medium"
+  },
+  {
+    "image": "yellow_p.png",
+    "sex": "female",
+    "size": "large"
+  },
+  {
+    "image": "yellow_p.png",
+    "sex": "male",
+    "size": "small"
+  },
+  {
+    "image": "yellow_p.png",
+    "sex": "male",
+    "size": "medium"
+  },
+  {
+    "image": "yellow_p.png",
+    "sex": "male",
+    "size": "large"
+  },
+  {
+    "image": "yellow_s.png",
+    "sex": "female",
+    "size": "small"
+  },
+  {
+    "image": "yellow_s.png",
+    "sex": "female",
+    "size": "medium"
+  },
+  {
+    "image": "yellow_s.png",
+    "sex": "female",
+    "size": "large"
+  },
+  {
+    "image": "yellow_s.png",
+    "sex": "male",
+    "size": "small"
+  },
+  {
+    "image": "yellow_s.png",
+    "sex": "male",
+    "size": "medium"
+  },
+  {
+    "image": "yellow_s.png",
+    "sex": "male",
+    "size": "large"
+  },
+  {
+    "image": "yellow_t.png",
+    "sex": "female",
+    "size": "small"
+  },
+  {
+    "image": "yellow_t.png",
+    "sex": "female",
+    "size": "medium"
+  },
+  {
+    "image": "yellow_t.png",
+    "sex": "female",
+    "size": "large"
+  },
+  {
+    "image": "yellow_t.png",
+    "sex": "male",
+    "size": "small"
+  },
+  {
+    "image": "yellow_t.png",
+    "sex": "male",
+    "size": "medium"
+  },
+  {
+    "image": "yellow_t.png",
+    "sex": "male",
+    "size": "large"
+  }
+]}
---
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -0,0 +1,3 @@
+const data = readTextFile("../data/data.json");
+let dataOrigin = JSON.parse(JSON.stringify(dataFile));
+console.log(dataOrigin);
