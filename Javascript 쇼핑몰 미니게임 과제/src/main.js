d2428617e7632403ef57d4fb54460f9216a2a231
yeonuKim <ywk0524@snu.ac.kr>
2024-01-03 10:44:52 +0900

    [handling] Add filter

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -18,6 +18,28 @@ function createHTMLString(item) {
     `;
 }
 
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+
+  console.log(items);
+  console.log(key);
+  const filtered = items.filter((item) => item[key] === value);
+  console.log(filtered);
+  displayItems(filtered);
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector("#header-logo");
+  const buttons = document.querySelector(".button-wrapper");
+  logo.addEventListener("click", () => displayItems(items));
+  buttons.addEventListener("click", (event) => onButtonClick(event, items));
+}
 loadItems()
   .then((items) => {
     displayItems(items);
