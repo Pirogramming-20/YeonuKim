a7e2207375412d56f2c906a9310d24d3759074d9
yeonuKim <ywk0524@snu.ac.kr>
2024-01-04 09:55:34 +0900

    [docs] Remove unnecessary functions and add data json file

--- 숫자야구 게임/src/script.js
+++ 숫자야구 게임/src/script.js
@@ -81,6 +81,7 @@ for (const sendBtn of sendBtns) {
     if (!inputErrFlag) {
       check_numbers(inputValues);
     }
+    saveItems();
 
     // Reset input
     input1.value = null;
@@ -193,4 +194,16 @@ function displayFail() {
     "beforeend",
     `<img id="success-img" src="assets/img/fail.png" />`
   );
+}
+
+// Add Records in json file
+function loadItems() {
+  return fetch("data/data.json")
+    .then((response) => response.json())
+    .then((json) => json.items);
+}
+
+function saveItems() {
+  const records = fetch("data/data.json");
+  console.log(records);
 }
