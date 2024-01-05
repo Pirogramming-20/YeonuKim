d62b4bbd98fa8e28cf4c51b7bffd38507fe56433
yeonuKim <ywk0524@snu.ac.kr>
2024-01-05 09:06:03 +0900

    [feat] Implement menu 1 (insert student)

--- python_problem/run.py
+++ python_problem/run.py
@@ -1,9 +1,11 @@
 #함수 이름은 변경 가능합니다.
 
 ##############  menu 1
-def insertStudent() :
-    #사전에 학생 정보 저장하는 코딩 
-    pass
+def insertStudent(name, midScore, finalScore) :
+    #사전에 학생 정보 저장하는 코딩
+    newStudent = Student(name, midScore, finalScore)
+    studentList.append(newStudent)
+
 
 ##############  menu 2
 def grading() :
@@ -25,15 +27,32 @@ class Student:
         self.__name = name
         self.__midScore = midScore
         self.__finalScore = finalScore
+        self.__grade = ""
     
     def getName(self):
         return self.__name
     
-    def getMidScore(self):
-        return self.__midScore
+    def getGrade(self):
+        return self.__grade
     
-    def getFinalScore(self):
-        return self.__finalScore
+    def isGrade(self):
+        if(self.__grade == ""):
+            return False
+        else:
+            return True
+    
+    def calcGrade(self):
+        averageScore = (self.__midScore + self.__finalScore) /2
+        if (averageScore >= 90):
+            self.__grade = 'A'
+        elif (averageScore >= 80):
+            self.__grade = 'B'
+        elif (averageScore >= 70):
+            self.__grade = 'C'
+        else:
+            self.__grade = 'D'
+
+        
     
 #학생 정보를 저장할 변수 초기화
 studentList = []
@@ -51,7 +70,6 @@ while True :
         #예외사항 처리(데이터 입력 갯수, 이미 존재하는 이름, 입력 점수 값이 양의 정수인지)
         #예외사항이 아닌 입력인 경우 1번 함수 호출
         userinput = input("Enter name mid-score final-score: ").split()
-        print(userinput)
         if(len(userinput) != 3):
             print("Num of data is not 3!")
         elif(userinput[1].find('-') != -1 or userinput[1].find('.') != -1 or userinput[2].find('-') != -1 or userinput[2].find('.') !=-1):
@@ -60,10 +78,15 @@ while True :
             name = userinput[0]
             midScore = int(userinput[1])
             finalScore = int(userinput[2])
-            for student in studentList:
-                if(student.getName() == name):
-                    print("Already exist name!")
-                else:
+            doubleFlag = False
+            if (len(studentList) == 0):
+                insertStudent(name, midScore, finalScore)
+            else:
+                for student in studentList:
+                    if(student.getName() == name):
+                        print("Already exist name!")
+                        doubleFlag = True
+                if(not doubleFlag):
                     insertStudent(name, midScore, finalScore)
 
     elif choice == "2" :
@@ -87,8 +110,11 @@ while True :
     elif choice == "5" :
         #프로그램 종료 메세지 출력
         #반복문 종료
+        print("Exit Program!")
+        break
         pass
 
     else :
         #"Wrong number. Choose again." 출력
+        print("Wrong number. Choose again.")
         pass
