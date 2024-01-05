d7fd741adff37820904566d65b8cc088eb701b83
yeonuKim <ywk0524@snu.ac.kr>
2024-01-05 20:35:39 +0900

    [feat] Make studentlist class and changeGradeCut function

--- python_problem/run.py
+++ python_problem/run.py
@@ -1,85 +1,89 @@
-#함수 이름은 변경 가능합니다.
-
-##############  menu 1
-def insertStudent(name, midScore, finalScore) :
-    #사전에 학생 정보 저장하는 코딩
-    newStudent = Student(name, midScore, finalScore)
-    studentList.append(newStudent)
-
-##############  menu 2
-def grading() :
-    #학점 부여 하는 코딩
-    for student in studentList:
-        student.calcGrade()
-
-##############  menu 3
-def printStudent() :
-    #출력 코딩
-    print("-----------------------------")
-    print(f"{'name':10} {'mid':10} {'final':10} {'grade':10}")
-    for student in studentList:
-        print(f"{student.getName():10} {str(student.getMidScore()):10} {str(student.getFinalScore()):10} {student.getGrade():10}")
-
-##############  menu 4
-def deleteStudent(name):
-    #학생 정보 삭제하는 코딩
-    for student in studentList:
-        if(student.getName() == name):
-            studentList.remove(student)
-def invalidName(name):
-    for nameChar in name:
-        if(nameChar != '0'
-           and nameChar != '1'
-           and nameChar != '2'
-           and nameChar != '3'
-           and nameChar != '4'
-           and nameChar != '5'
-           and nameChar != '6'
-           and nameChar != '7'
-           and nameChar != '8'
-           and nameChar != '9'):
-            return True
-    return False
 class Student:
+    A_cut = 90
+    B_cut = 80
+    C_cut = 70
+
     def __init__(self, name, midScore, finalScore):
         self.__name = name
         self.__midScore = midScore
         self.__finalScore = finalScore
-        self.__grade = ""
-    
+        self.__grade = ""    
     def getName(self):
-        return self.__name
-    
+        return self.__name    
     def getMidScore(self):
-        return self.__midScore
-    
+        return self.__midScore    
     def getFinalScore(self):
-        return self.__finalScore
-    
+        return self.__finalScore    
     def getGrade(self):
-        return self.__grade
-    
+        return self.__grade    
     def isGrade(self):
         if(self.__grade == ""):
             return False
         else:
-            return True
-    
+            return True    
     def calcGrade(self):
         averageScore = (self.__midScore + self.__finalScore) /2
-        if (averageScore >= 90):
+        if (averageScore >= self.A_cut):
             self.__grade = 'A'
-        elif (averageScore >= 80):
+        elif (averageScore >= self.B_cut):
             self.__grade = 'B'
-        elif (averageScore >= 70):
+        elif (averageScore >= self.C_cut):
             self.__grade = 'C'
         else:
             self.__grade = 'D'
 
-        
-    
-#학생 정보를 저장할 변수 초기화
-studentList = []
+    # Add function that can change grade cut using classmethod
+    @classmethod
+    def changeGradeCut(cls, A_cut, B_cut, C_cut):
+        cls.A_cut = A_cut
+        cls.B_cut = B_cut
+        cls.C_cut = C_cut
+
+class StudentList:
+    def __init__(self):
+        self.__students = []
+
+    def isEmpty(self):
+        return not self.__students  
+      
+    def getStudentList(self):
+        return self.__students   
+     
+    def insertStudent(self, name, midScore, finalScore) :
+        newStudent = Student(name, midScore, finalScore)
+        self.__students.append(newStudent)
+
+    def grading(self):
+        for student in self.__students:
+            student.calcGrade()
+
+    def printStudents(self):
+        print("---------------------------------------")
+        print(f"{'name':10} {'mid':10} {'final':10} {'grade':10}")
+        for student in self.__students:
+            print(f"{student.getName():10} {str(student.getMidScore()):10} {str(student.getFinalScore()):10} {student.getGrade():10}")
+
+    def deleteStudent(self, name):
+        for student in self.__students:
+            if(student.getName() == name):
+                self.__students.remove(student)
+
+def invalidScore(score):
+    for scoreChar in score:
+        if(scoreChar != '0'
+           and scoreChar != '1'
+           and scoreChar != '2'
+           and scoreChar != '3'
+           and scoreChar != '4'
+           and scoreChar != '5'
+           and scoreChar != '6'
+           and scoreChar != '7'
+           and scoreChar != '8'
+           and scoreChar != '9'):
+            return True
+    return False
+
+students = StudentList()
 print("*Menu*******************************")
 print("1. Inserting students Info(name score1 score2)")
 print("2. Grading")
@@ -88,78 +92,79 @@ print("4. Deleting students Info")
 print("5. Exit program")
 print("*************************************")
 while True :
-    choice = input("Choose menu 1, 2, 3, 4, 5 : ")
+    choice = input("Choose menu 1, 2, 3, 4, 5, 6 : ")
     if choice == "1":
-        #학생 정보 입력받기
-        #예외사항 처리(데이터 입력 갯수, 이미 존재하는 이름, 입력 점수 값이 양의 정수인지)
-        #예외사항이 아닌 입력인 경우 1번 함수 호출
         userinput = input("Enter name mid-score final-score: ").split()
         if(len(userinput) != 3):
             print("Num of data is not 3!")
             continue
-        if(invalidName(userinput[1]) or invalidName(userinput[2])):
+        if(invalidScore(userinput[1]) or invalidScore(userinput[2])):
             print("Score is not positive integer!")
             continue
         name = userinput[0]
         midScore = int(userinput[1])
         finalScore = int(userinput[2])
         doubleFlag = False
-        for student in studentList:
+        for student in students.getStudentList():
             if(student.getName() == name):
                 print("Already exist name!")
                 doubleFlag = True
         if(not doubleFlag):
-            insertStudent(name, midScore, finalScore)
+            students.insertStudent(name, midScore, finalScore)
 
     elif choice == "2" :
-        #예외사항 처리(저장된 학생 정보의 유무)
-        #예외사항이 아닌 경우 2번 함수 호출
-        #"Grading to all students." 출력
-        if(len(studentList)==0):
+        if(students.isEmpty()):
             print("No student data!")
             continue
-        grading()
+        students.grading()
         print("Grading to all students.")
 
     elif choice == "3" :
-        #예외사항 처리(저장된 학생 정보의 유무, 저장되어 있는 학생들의 학점이 모두 부여되어 있는지)
-        #예외사항이 아닌 경우 3번 함수 호출
         isGradeFlag = False
-        if(len(studentList)==0):
+        if(students.isEmpty()):
             print("No student data!")
             continue
-        for student in studentList:
+        for student in students.getStudentList():
             if(not student.isGrade()):
                 print("There is a student who didn't get grade.")
                 isGradeFlag = True
                 break               
         if(not isGradeFlag):
-            printStudent()
+            students.printStudents()
 
     elif choice == "4" :
-        #예외사항 처리(저장된 학생 정보의 유무)
-        #예외사항이 아닌 경우, 삭제할 학생 이름 입력 받기
-        #입력 받은 학생의 존재 유무 체크 후, 없으면 "Not exist name!" 출력
-        #있으면(예를 들어 kim 이라 하면), 4번 함수 호출 후에 "kim student information is deleted." 출력
         targetName = input("Enter the name to delete: ")
         existStudentFlag = False
-        if(len(studentList) == 0):
+        if(students.isEmpty()):
             print("No student data!")
             continue
-        for student in studentList:
+        for student in students.getStudentList():
             if(student.getName() == targetName):
                 existStudentFlag = True
-                deleteStudent(targetName)
+                students.deleteStudent(targetName)
         if(not existStudentFlag):
             print("Not exist name!")
         pass
 
     elif choice == "5" :
-        #프로그램 종료 메세지 출력
-        #반복문 종료
         print("Exit Program!")
         break
 
+    # Additinal function: change the score cut of grade
+    elif choice == "6" :
+        userinput = input("Enter the score cut of A, B, and C: ").split()
+        print(len(userinput))
+        if(len(userinput) != 3):
+            print("Num of data is not 3!")
+            continue
+        if(invalidScore(userinput[0]) or invalidScore(userinput[1]) or invalidScore(userinput[2])):
+            print("Score is not positive integer!")
+            continue
+        A_cut, B_cut, C_cut = map(int, userinput)
+        if (A_cut < B_cut or B_cut < C_cut):
+            print("Score cut is invalid.")
+            continue
+        Student.changeGradeCut(A_cut, B_cut, C_cut)
+
     else :
-        #"Wrong number. Choose again." 출력
         print("Wrong number. Choose again.")
