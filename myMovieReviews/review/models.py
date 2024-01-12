349b681b2f042d5f6edb29ec50596fd2ef7017e1
yeonuKim <ywk0524@snu.ac.kr>
2024-01-12 16:40:35 +0900

    [feat] Add choices in createYear and Genres

--- myMovieReviews/review/models.py
+++ myMovieReviews/review/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 
 # Create your models here.
+GENRES_CHOICES=(
+    ('SF', 'SF'),
+    ('ROMANCE', 'Romance'),
+    ('COMEDY', 'Comedy'),
+    ('ACTION', 'Action'),
+    ('MUSIC', 'Music'),
+    ('HORROR', 'Horror'),
+)
+
+YEAR_CHOICES_LIST=[]
+for i in range(1950, 2025):
+    YEAR_CHOICES_LIST.append((i, i))
+YEAR_CHOICES = tuple(YEAR_CHOICES_LIST)
+
 class Review(models.Model):
     title = models.CharField(max_length=200)
-    createYear = models.IntegerField()
-    genres = models.CharField(max_length=200)
+    createYear = models.IntegerField(choices = YEAR_CHOICES)
+    genres = models.CharField(max_length=200, choices=GENRES_CHOICES)
     starRate = models.DecimalField(max_digits=2, decimal_places=1)
     runningTime = models.IntegerField()
     content = models.TextField(max_length=10000)
