047d9e4ba3dfbb8c0ef5676c1c145a58b059ac56
yeonuKim <ywk0524@snu.ac.kr>
2024-01-15 23:26:23 +0900

    [feat] Make following using manytomany field

--- SellingCommunity_ERD/common/urls.py
+++ SellingCommunity_ERD/common/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import signup, logout_page
+from .views import signup, logout_page, mypage, follow
 
 app_name = 'common'
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
-    path('logout/', logout_page, name='logout')
+    path('logout/', logout_page, name='logout'),
+    path('mypage/', mypage, name='user_info'),
+    path('mypage/follow/<int:pk>', follow, name='follow')
 ]
--- SellingCommunity_ERD/common/views.py
+++ SellingCommunity_ERD/common/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, logout
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+from market.models import User
 from .forms import UserForm, AddressForm
 
 # Create your views here.
@@ -23,3 +25,21 @@ def signup(request):
 def logout_page(request):
     logout(request)
     return redirect('market:index')
+
+def mypage(request):
+    me = request.user
+    users = User.objects.all()
+    return render(request, 'common/user_info.html', {'me': me, 'users': users})
+
+@login_required(login_url='common:login')
+def follow(request, pk):
+    target = get_object_or_404(User, pk=pk)
+    if request.method == 'POST':
+        if target != request.user:
+            if request.user.following.filter(pk=pk).exists():
+                request.user.following.remove(target)
+            else:
+                request.user.following.add(target)
+        request.user.save()
+    return redirect('common:user_info')
+
